76a25c37889b096a36ef74ba7732922a830b9c73
TalaatEysa <talaateysa@outlook.com>
2025-02-07 23:09:16 +0200

    substring search using kmp algorithm

--- 1-practice/14-countSubString.js
+++ 1-practice/14-countSubString.js
@@ -14,3 +14,54 @@ function countSubstring(longStr, shortStr) {
 }
 
 console.log(countSubstring('sopranos thanos', 'os'));
+
+//Using KMP Algorithm
+
+//ABBAC
+//00010
+function matchTable(pattern) {
+    let lps = Array.from({ length: pattern.length }).fill(0);
+    let suffixEnd = 1;
+    let prefixEnd = 0;
+    while (suffixEnd < pattern.length) {
+        if (pattern[suffixEnd] === pattern[prefixEnd]) {
+            prefixEnd++;
+            lps[suffixEnd] = prefixEnd;
+            suffixEnd++;
+        } else if (prefixEnd !== 0) {
+            prefixEnd = lps[prefixEnd - 1];
+        } else {
+            lps[suffixEnd] = 0;
+            suffixEnd++;
+        }
+    }
+    return lps;
+}
+
+function kmpSearch(text, pattern) {
+    let lpsTable = matchTable(pattern);
+    let patternIndex = 0;
+    let textIndex = 0;
+    let count = 0;
+
+    while (textIndex < text.length) {
+        if (text[textIndex] === pattern[patternIndex]) {
+            textIndex++;
+            patternIndex++;
+
+            if (patternIndex === pattern.length) {
+                count++;
+                patternIndex = lpsTable[patternIndex - 1];
+            }
+        } else {
+            if (patternIndex !== 0) {
+                patternIndex = lpsTable[patternIndex - 1];
+            } else {
+                textIndex++;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(kmpSearch('hello heap', 'hea'));
