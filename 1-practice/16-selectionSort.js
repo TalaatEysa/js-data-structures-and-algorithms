155fe91174011aa8b168a3857d8e073b865a80af
TalaatEysa <talaateysa@outlook.com>
2025-02-14 00:25:12 +0200

    selection sort

---
+++ 1-practice/16-selectionSort.js
@@ -0,0 +1,21 @@
+// Implement Selection Sort
+
+function selectionSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+        if (i !== lowest) {
+            swap(arr, i, lowest);
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort([20, 62, 25, 12, 35]));
