cd27b61f139a4235606f4f3f3b42d6c22ee52a9a
TalaatEysa <talaateysa@outlook.com>
2025-03-01 23:08:15 +0200

    [Singly-Linked-List] implement push method

--- 2-Data-Structures/Singly-Linked-List.js
+++ 2-Data-Structures/Singly-Linked-List.js
@@ -11,4 +11,19 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+const ne = new SinglyLinkedList();
+console.log(ne.push('hi'));
+console.log(ne.push('hello'));
